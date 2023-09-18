7a9e68e963eaceff92e276d474278679bc5fdbd6
manish <manish@gmail.com>
2023-09-18 22:16:55 +0530

    addde

--- dsa_code/arraylist.java
+++ dsa_code/arraylist.java
@@ -6,7 +6,7 @@ public class arraylist {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
 
-        // add the arraylist
+       // array list
         list.add(5);
         list.add(1);
         list.add(8);
